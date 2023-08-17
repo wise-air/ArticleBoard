414cd862b33ab46897dcbac6ce8f3973c26f7509
Ilfat Arslanov <123643044+wise-air@users.noreply.github.com>
2023-08-18 00:09:28 +0500

    Delete secret.py

--- secret.py
+++
@@ -1,9 +0,0 @@
-from articleBoard.settings import *
-
-EMAIL_FOR_SEND = "wisears"
-
-EMAIL_FOR_SEND_PASS = "vbvzrtfxprrcjoyp"
-
-# EMAIL_HOST_USER = "wisears@yandex.ru"
-
-KEY_FOR_IFRAME = '8abf1f6dd588467e7a8d7c'
